0d330475dc515a84f79f0ab95efaaa656801ae4d
tansim-nabil <tansim.nabil@gmail.com>
2023-03-22 16:44:25 +0600

    interface added in repository layer

---
+++ IMS_Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using IMS_DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS_Repository
+{
+    public interface IRepository<T> where T : BaseEntity 
+    {
+        IEnumerable<T> GetAll();
+        T Get(int Id);
+    }
+}
---
+++ IMS_Repository/Repository.cs
@@ -0,0 +1,32 @@
+﻿using IMS_DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS_Repository
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly ApplicationContext _context;
+
+        public Repository(ApplicationContext context)
+        {
+            _context = context;
+            entity = _context.Set<T>();
+        }
+
+        private readonly DbSet<T> entity;
+        public T Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
