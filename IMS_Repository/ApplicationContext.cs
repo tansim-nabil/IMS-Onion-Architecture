c345ccb9f6af6fc12fddbd4dcdb610db0c987d44
tansim-nabil <tansim.nabil@gmail.com>
2023-03-22 16:04:46 +0600

    ApplicationContext created in Repository layer

---
+++ IMS_Repository/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using IMS_DataAccess.Account;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS_Repository
+{
+    public class ApplicationContext : IdentityDbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+      
+    }
+}
